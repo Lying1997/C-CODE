cbbb606ce40c3659763435b75c9e62e10556867a
Lying1997 <980400438Q@qq.com>
2022-07-04 14:33:39 +0800

    c

---
+++ Project1/Project1/7.3.1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+
+	printf("lili\n");
+
+
+	printf("%d\n", sizeof(int));
+
+	printf("%d\n", sizeof(short));
+
+	printf("%d\n", sizeof(long));
+
+	printf("%d\n", sizeof(long long));
+
+	printf("%d\n", sizeof(float));
+
+
+
+	return 0;
+
+}
+
+
